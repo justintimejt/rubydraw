0a609922a8dd91ab580e0382219594cf3540ffb4
Justin Chow <justinchow3607@gmail.com>
2026-01-03 17:35:43 -0500

    tldraw license

--- frontend/app/components/TldrawBoard.tsx
+++ frontend/app/components/TldrawBoard.tsx
@@ -1,6 +1,8 @@
+import * as React from "react";
 import type { Editor } from "tldraw";
 import { Tldraw } from "tldraw";
 import "tldraw/tldraw.css";
+import { getAssetUrlsByImport } from "@tldraw/assets/imports.vite";
 
 export function TldrawBoard({
   persistenceKey,
@@ -9,10 +11,15 @@ export function TldrawBoard({
   persistenceKey: string;
   onEditor?: (editor: Editor) => void;
 }) {
+  const assetUrls = React.useMemo(() => getAssetUrlsByImport(), []);
+  const licenseKey = import.meta.env.VITE_TLDRAW_LICENSE_KEY;
+
   return (
     <div style={{ position: "absolute", inset: 0 }}>
       <Tldraw
         persistenceKey={persistenceKey}
+        assetUrls={assetUrls}
+        licenseKey={licenseKey}
         onMount={(editor) => {
           onEditor?.(editor);
         }}
